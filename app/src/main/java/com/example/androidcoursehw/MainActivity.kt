ab70b910e99e7a9104ad06a1f27b328a0d7acceb
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-11-04 18:19:47 +0300

    nav graph ang controller

--- app/src/main/java/com/example/androidcoursehw/MainActivity.kt
+++ app/src/main/java/com/example/androidcoursehw/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.androidcoursehw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var controller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        controller = findController(R.id.nav_host_fragment_container)
     }
 }
