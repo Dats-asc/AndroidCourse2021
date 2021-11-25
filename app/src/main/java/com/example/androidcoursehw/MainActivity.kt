4a15d953f8bdd69966c0748dc8dfe4b16114c6a0
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2021-11-25 14:46:29 +0300

    done hw

--- app/src/main/java/com/example/androidcoursehw/MainActivity.kt
+++ app/src/main/java/com/example/androidcoursehw/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.androidcoursehw
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidcoursehw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var controller: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
 
         controller = findController(R.id.nav_host_fragment_container)
+        binding.bnvMain.setupWithNavController(controller)
     }
 }
